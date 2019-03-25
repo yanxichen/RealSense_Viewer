55d3ed0115ec6cf571b94344f20c28da93440515
Yanxi Chen <9inecats@gmail.com>
2019-03-25 12:15:51 -0600

    Added multi-viewport

--- scripts/realsense_viewer.py
+++ scripts/realsense_viewer.py
@@ -7,49 +7,67 @@ import time
 
 
 class VtkPointCloud:
-    def __init__(self, color=False, zMin=0.0, zMax=255.0, maxNumPoints=1e10):
+    def __init__(self, mode=0, zMin=0.0, zMax=255.0, maxNumPoints=1e10):
+        """
+        :param mode: 0 - Stereo Mode
+                     1 - RGB Mode
+                     2 - Both
+        """
+        self.mode = mode
         self.maxNumPoints = maxNumPoints
-        self.vtkPolyData = vtk.vtkPolyData()
-        self.color = color
-        mapper = vtk.vtkPolyDataMapper()
-        if self.color is False:
-            mapper.SetColorModeToDefault()
-            mapper.SetScalarRange(zMin, zMax)
-        mapper.SetScalarVisibility(1)
-        mapper.SetInputData(self.vtkPolyData)
-        self.clearPoints()
-        self.vtkActor = vtk.vtkActor()
-        self.vtkActor.SetMapper(mapper)
+        self.stereo_vtkPolyData = vtk.vtkPolyData()
+        self.rgb_vtkPolyData = vtk.vtkPolyData()
+        stereo_mapper = vtk.vtkPolyDataMapper()
+        rgb_mapper = vtk.vtkPolyDataMapper()
+
+        if self.mode not in {0, 1, 2}:
+            raise ValueError('Invalid Mode. Choose from 0 (Stereo Mode), 1 (RGB Mode), 2 (Both)')
+        if self.mode in {0, 2}:  # Stereo
+            stereo_mapper.SetColorModeToDefault()
+            stereo_mapper.SetScalarRange(zMin, zMax)
+            stereo_mapper.SetScalarVisibility(1)
+            stereo_mapper.SetInputData(self.stereo_vtkPolyData)
+            self.stereo_vtkActor = vtk.vtkActor()
+            self.stereo_vtkActor.SetMapper(stereo_mapper)
+        if self.mode in {1, 2}:  # RGB
+            rgb_mapper.SetScalarVisibility(1)
+            rgb_mapper.SetInputData(self.rgb_vtkPolyData)
+            self.rgb_vtkActor = vtk.vtkActor()
+            self.rgb_vtkActor.SetMapper(rgb_mapper)
+
         self.numPoints = 0  # For debugging
+        self.clearPoints()
 
     def addPoint(self, point):
         pointId = self.vtkPoints.InsertNextPoint([640 - point[0], 480 - point[1], 0 - point[2]])
         self.vtkDepth.InsertNextValue(point[2])
         self.vtkCells.InsertNextCell(1)
         self.vtkCells.InsertCellPoint(pointId)
-        self.vtkPolyData.Modified()
         self.vtkCells.Modified()
         self.vtkPoints.Modified()
         self.vtkDepth.Modified()
-        # self.numPoints += 1
+        self.stereo_vtkPolyData.Modified()
+        self.rgb_vtkPolyData.Modified()
 
     def clearPoints(self):
         self.vtkPoints = vtk.vtkPoints()
         self.vtkCells = vtk.vtkCellArray()
         self.vtkDepth = vtk.vtkDoubleArray()
         self.vtkDepth.SetName('DepthArray')
-        self.vtkPolyData.SetPoints(self.vtkPoints)
-        self.vtkPolyData.SetVerts(self.vtkCells)
-        if self.color is False:
-            self.vtkPolyData.GetPointData().SetScalars(self.vtkDepth)
-            self.vtkPolyData.GetPointData().SetActiveScalars('DepthArray')
-        else:
+        self.stereo_vtkPolyData.SetPoints(self.vtkPoints)
+        self.stereo_vtkPolyData.SetVerts(self.vtkCells)
+        self.rgb_vtkPolyData.SetPoints(self.vtkPoints)
+        self.rgb_vtkPolyData.SetVerts(self.vtkCells)
+
+        if self.mode in {1, 2}:  # RGB
             self.Colors = vtk.vtkUnsignedCharArray()
             self.Colors.SetNumberOfComponents(3)
             self.Colors.SetName("Colors")
-            self.vtkPolyData.GetPointData().SetScalars(self.Colors)
-            self.vtkPolyData.GetPointData().SetActiveScalars('Colors')
-        # self.numPoints = 0
+            self.rgb_vtkPolyData.GetPointData().SetScalars(self.Colors)
+            self.rgb_vtkPolyData.GetPointData().SetActiveScalars('Colors')
+        if self.mode in {0, 2}:  # Stereo
+            self.stereo_vtkPolyData.GetPointData().SetScalars(self.vtkDepth)
+            self.stereo_vtkPolyData.GetPointData().SetActiveScalars('DepthArray')
 
     def update_pointcloud(self, threadLock, update_on):
         while update_on.is_set():
@@ -68,11 +86,10 @@ class VtkPointCloud:
                         continue
                     dist = depth_frame.get_distance(x, y) * 50
                     self.addPoint([x, y, dist])
-                    if self.color:
+                    if self.mode in {1, 2}:  # RGB
                         rgb = color_data[y, x]
                         self.Colors.InsertNextTuple3(rgb[0], rgb[1], rgb[2])
             threadLock.release()
-            # print(self.numPoints)
 
     def update(self, threadLock, update_on):
         thread = threading.Thread(target=self.update_pointcloud, args=(threadLock, update_on))
@@ -84,15 +101,27 @@ class Visualization:
         self.threadLock = threadLock
         self.iterations = iterations
         self.pointCloud = pointCloud
-
-        self.renderer = vtk.vtkRenderer()
-        self.renderer.SetBackground(.2, .3, .4)
-        self.renderer.ResetCamera()
-        self.renderer.AddActor(pointCloud.vtkActor)
-
+        self.mode = pointCloud.mode
         self.renderWindow = vtk.vtkRenderWindow()
         self.renderWindow.SetSize(1000, 800)
-        self.renderWindow.AddRenderer(self.renderer)
+
+        if self.mode in {0, 2}:  # Stereo
+            self.stereo_renderer = vtk.vtkRenderer()
+            self.stereo_renderer.SetBackground(.2, .3, .4)
+            self.stereo_renderer.ResetCamera()
+            self.stereo_renderer.AddActor(pointCloud.stereo_vtkActor)
+            self.renderWindow.AddRenderer(self.stereo_renderer)
+
+        if self.mode in {1, 2}:  # RGB
+            self.rgb_renderer = vtk.vtkRenderer()
+            self.rgb_renderer.SetBackground(.2, .3, .4)
+            self.rgb_renderer.ResetCamera()
+            self.rgb_renderer.AddActor(pointCloud.rgb_vtkActor)
+            self.renderWindow.AddRenderer(self.rgb_renderer)
+
+        if self.mode == 2:  # Both
+            self.stereo_renderer.SetViewport(0, 0, 0.5, 1)
+            self.rgb_renderer.SetViewport(0.5, 0, 1, 1)
 
         # Interactor
         self.renderWindowInteractor = vtk.vtkRenderWindowInteractor()
@@ -105,22 +134,30 @@ class Visualization:
     def update_visualization(self, obj=None, event=None):
         time.sleep(0.01)
         self.threadLock.acquire()
-        self.renderer.GetRenderWindow().Render()
-        if self.iterations == 30:
-            self.renderer.ResetCamera()
-            self.iterations -= 1
+        if self.mode in {0, 2}:  # Stereo
+            self.stereo_renderer.GetRenderWindow().Render()
+            if self.iterations == 30:
+                self.stereo_renderer.ResetCamera()
+        if self.mode in {1, 2}:  # RGB
+            self.rgb_renderer.GetRenderWindow().Render()
+            if self.iterations == 30:
+                self.rgb_renderer.ResetCamera()
+        self.iterations -= 1
         self.threadLock.release()
+
+
+def run(mode=0):
+    update_on = threading.Event()
+    update_on.set()
+    threadLock = threading.Lock()
+
+    pointCloud = VtkPointCloud(mode)
+    pointCloud.update(threadLock, update_on)
+
+    visual = Visualization(threadLock, pointCloud, 30)
+    visual.renderWindowInteractor.Start()
 
 
 pipeline = rs.pipeline()
 pipeline.start()
-
-update_on = threading.Event()
-update_on.set()
-threadLock = threading.Lock()
-
-pointCloud = VtkPointCloud(color=True)  # Default to heatmap when color=False
-pointCloud.update(threadLock, update_on)
-
-visual = Visualization(threadLock, pointCloud, 30)
-visual.renderWindowInteractor.Start()
+run()
